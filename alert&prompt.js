69d843eb46f08d42f5e2cec9c00d8308076fed91
João Vitor Madureira <103207658+j-madureira@users.noreply.github.com>
2022-04-11 16:49:57 -0300

    Add files via upload

---
+++ alert&prompt.js
@@ -0,0 +1,5 @@
+/*Receber os dados via prompt e retornar a variável com uma frase de boa tarde 
+  no alert com o valor do prompt
+*/
+var nome = prompt("Olá digite seu nome para receber uma mensagem de boa tarde:");
+alert("Olá ! "  +  nome +   " ,boa tarde espero que esteja bem :)");
